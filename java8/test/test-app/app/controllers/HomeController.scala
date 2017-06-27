d810a5b4fbe3d4b381aec70bbdb63c3941c62728
Yoran van Oirschot <yoran@building-blocks.nl>
2017-06-27 15:18:11 +0200

    New message

--- java8/test/test-app/app/controllers/HomeController.scala
+++ java8/test/test-app/app/controllers/HomeController.scala
@@ -18,7 +18,7 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Hi BBlocks! Whatsup bitches!"))
+    Ok(views.html.index("Hi BBlocks! Whatsup bitches! Aap!"))
   }
 
 }
